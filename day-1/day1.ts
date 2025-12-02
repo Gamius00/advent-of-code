f7dd8724fff2ddcb5c924f0d2f7991ac62c51134
fas <Fabius.Schurig@communardo.de>
2025-12-02 17:19:53 +0100

    first part of day 1

---
+++ day-1/day1.ts
@@ -0,0 +1,35 @@
+const text = await Deno.readTextFile("day-1/input-day1.txt");
+const array = text.toString().split((/\s+/))
+let count = 50
+let password = 0
+
+// Rotation of the safe
+const rotate = (direction, drehung) => {
+    if (direction === "R") {
+        const test = (count % 100) + Number(drehung)
+        if (test === 100) {
+            count = 0
+        }
+        count = test % 100
+
+        if (count === 0) {
+            password = password + 1
+        }
+    }
+
+    else {
+        const test = drehung % 100
+        count = ((100 - test) + count) % 100
+
+        if (count === 0) {
+            password = password + 1
+        }
+    }
+}
+
+array.map((item) => {
+    const itemparts = item.split("")
+    rotate(itemparts[0], itemparts.slice(1).join(""))
+})
+
+console.log(password)
