b102d4ff821c14cfe58b69d87a5f3bb63b16ca97
fas <Fabius.Schurig@communardo.de>
2025-12-03 11:32:15 +0100

    first part of day 2

---
+++ day-2/day2-0.ts
@@ -0,0 +1,21 @@
+const text = await Deno.readTextFile("day-2/input.txt");
+const array = text.toString().split(",")
+let invalidIds = 0
+
+array.map((item) => {
+    const startEnd = item.split("-").map(number => {return number})
+
+    for (let step = Number(startEnd[0]); step <= Number(startEnd[1]); step++) {
+        if (step.toString().split("").length % 2 === 0) {
+            const test = step.toString().split("").length / 2
+            const part1 = Number(step.toString().slice(0, test))
+            const part2 = Number(step.toString().slice(test))
+
+            if (part1 === part2) {
+                invalidIds = invalidIds + Number(part1.toString() + part2.toString())
+            }
+        }
+    }
+})
+
+console.log(invalidIds)
